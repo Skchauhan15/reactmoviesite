9a84cd0a57686ea088d9cf37018f706a52086fbe
Sumit Kumar <112681701+Skchauhan15@users.noreply.github.com>
2022-12-12 00:39:04 +0530

    Add files via upload

---
+++ moviesite_React_Project/moviesite/src/component/Footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class Footer extends Component {
+  render() {
+    return (
+      <div className='footer'>
+       <div>© 2022 Sumit Kumar</div>
+       <div>skchauhan15101996@gmail.com</div>
+       <div></div>
+      </div>
+    )
+  }
+}
